1045be9d5cc69d705c22ed70064ed3022b31ab3c
Siddharth Hota <siddharthhota26@gmail.com>
2023-07-03 13:17:59 +0530

    initial

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,37 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [todo, setTodo] = useState('');
+  let [list, setList] = useState([]);
+  let [id, setId] = useState(0);
+
+  function addToList() {
+    setId(id + 1);
+    let obj = { 'id': id, 'todo': todo }
+    setList([...list, obj])
+  }
+
+  function deleteTask(id){
+    setList(list.filter((todo)=>(todo.id!==id)));
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h3>To-Do List</h3>
+      <input type='text' onChange={(e) => { setTodo(e.target.value) }}></input>
+      <button onClick={addToList}>Add Todo</button>
+      {
+        list.length > 0 &&
+        list.map((task) => (
+          <div>
+            <span>{task.todo}</span>
+            <button onClick={()=>{deleteTask(task.id)}}>Delete</button>
+          </div>
+        ))
+      }
     </div>
   )
 }
